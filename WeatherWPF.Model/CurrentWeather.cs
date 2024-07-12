03bd5e49f5b7500e1fae14283bb0871960da35a9
Alexey <com.twin@yandex.ru>
2024-07-12 22:23:39 +0700

    Добавьте файлы проекта.

---
+++ WeatherWPF.Model/CurrentWeather.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherWPF.Model
+{
+    public class CurrentWeather
+    {
+        [JsonProperty("temp_c")]
+        public double TempC { get; set; }
+
+        [JsonProperty("condition")]
+        public Condition Condition { get; set; }
+
+        [JsonProperty("wind_kph")]
+        public double WindKph { get; set; }
+
+        [JsonProperty("humidity")]
+        public double Humidity { get; set; }
+    }
+}
---
+++ WeatherWPF.Model/Location.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherWPF.Model
+{
+    public class Location
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
