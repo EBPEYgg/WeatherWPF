1c018164d0b91f270eaa552a9431af815e86dd27
Alexey <com.twin@yandex.ru>
2024-07-13 18:54:27 +0700

    feature(ViewModel & View): add a display of temperature, humidity, wind speed and clouds depending on the city

--- WeatherWPF.ViewModel/MainVM.cs
+++ WeatherWPF.ViewModel/MainVM.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json;
 using System.Net;
 using WeatherWPF.Model;
 
@@ -11,9 +12,9 @@ namespace WeatherWPF.ViewModel
     public partial class MainVM : ObservableObject
     {
         /// <summary>
-        /// API key.
+        /// API ключ сервиса погоды.
         /// </summary>
-        private const string _apiKey = "9fbca1cd22b74a98808145032241207";
+        private const string _apiKeyWeather = "9fbca1cd22b74a98808145032241207";
 
         /// <summary>
         /// Текущее время.
@@ -40,9 +41,37 @@ namespace WeatherWPF.ViewModel
         private double _humidity;
 
         /// <summary>
+        /// Преобразованное значение влажности для слайдера (от 0 до 10).
+        /// </summary>
+        [ObservableProperty]
+        private double _humiditySlider;
+
+        /// <summary>
+        /// Количество градусов по Цельсию.
+        /// </summary>
+        [ObservableProperty]
+        private string _tempC;
+
+        /// <summary>
+        /// Облачность.
+        /// </summary>
+        [ObservableProperty]
+        private string _conditionText;
+
+        /// <summary>
+        /// Изображение облачности.
+        /// </summary>
+        [ObservableProperty]
+        private string _conditionIcon;
+
+        /// <summary>
         /// Возвращает и задает команду для выхода из приложения.
         /// </summary>
         public RelayCommand QuitCommand { get; set; }
+
+        public RelayCommand GetWeatherCommand { get; set; }
+
+        public RelayCommand ConvertToFahrenheit { get; set; }
 
         /// <summary>
         /// Конструктор класса <see cref="MainVM"/>.
@@ -50,10 +79,13 @@ namespace WeatherWPF.ViewModel
         public MainVM()
         {
             QuitCommand = new RelayCommand(Quit);
+            GetWeatherCommand = new RelayCommand(GetWeather);
             DateTimeNow = DateTime.Now;
+            CityName = "London";
+            GetWeatherCommand.Execute(this);
         }
 
-        /// <inheritdoc cref="Application.Shutdown()"/>
+        /// <inheritdoc cref="Environment.Exit(int)"/>
         private void Quit()
         {
             Environment.Exit(0);
@@ -71,7 +103,7 @@ namespace WeatherWPF.ViewModel
                 return;
             }
 
-            string apiUrl = $"http://api.weatherapi.com/v1/current.json?key={_apiKey}&q={CityName}";
+            string apiUrl = $"http://api.weatherapi.com/v1/current.json?key={_apiKeyWeather}&q={CityName}";
 
             try
             {
@@ -85,8 +117,8 @@ namespace WeatherWPF.ViewModel
                         using (StreamReader reader = new StreamReader(stream))
                         {
                             string jsonResponse = reader.ReadToEnd();
-                            //WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(jsonResponse);
-                            //DisplayWeatherData(weatherData);
+                            WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(jsonResponse);
+                            DisplayWeatherData(weatherData);
                         }
                     }
                 }
@@ -99,9 +131,12 @@ namespace WeatherWPF.ViewModel
 
         private void DisplayWeatherData(WeatherData weatherData)
         {
-            CityName = weatherData.Location.Name;
+            TempC = Convert.ToInt32(weatherData.CurrentWeather.TempC) + "°c";
             WindSpeed = weatherData.CurrentWeather.WindKph;
             Humidity = weatherData.CurrentWeather.Humidity;
+            HumiditySlider = Humidity / 10;
+            ConditionText = weatherData.CurrentWeather.Condition.Text;
+            ConditionIcon = "https:" + weatherData.CurrentWeather.Condition.Icon;
         }
     }
 }
